06ba91b6d8f7185cf6b460e699b626b7deebd876
suichanwa <augustinnegura746@gmail.com>
2023-10-11 22:39:38 +0300

    update

---
+++ Leson1/src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/Mancare_calda', (req, res) => {
+  res.sendFile(__dirname + 'Pages/Mancare_calda.html');
+});
+
+app.get('/Mancare_rece', (req, res) => {
+  res.sendFile(__dirname + 'Pages/Mancare_rece.html');
+});
+
+app.get('/Despre_noi', (req, res) => {
+  res.sendFile(__dirname + 'Pages/Despre_noi.html');
+});
+
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/Pages/contact.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+//write a function that output things 
