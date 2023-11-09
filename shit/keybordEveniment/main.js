2c7c7aaa7c724b19c72cadc8260a52835d0027dd
suichanwa <augustinnegura746@gmail.com>
2023-11-09 03:18:33 -0800

    upd

--- shit/keybordEveniment/main.js
+++ shit/keybordEveniment/main.js
@@ -1,56 +1,49 @@
-/*
-const red = document.body.querySelector(".red");
-const blue = document.body.querySelector(".blue");
-
-const win = window.addEventListener("keydown", (ev) => {
-    if(ev.keyCode === 37){
-       red.classList.add("blue");
-    } else if (ev.keyCode === 39){
-        blue.classList.remove("blue");
-        blue.classList.add("red");
-    }
-});
-*/
 const boy = document.getElementById('boy');
 const getDistance = () => {return window.parseInt(boy.style.transform.replace('translateX(', ''), 10) || 0;};
+let distX = getDistance() + 30;
+let distY = getDistance() + -30;
+const screenWidth = window.innerWidth;
 
-const moveRight = () =>{
+
+const moving = () =>{
     window.addEventListener("keydown", (ev) => {
         if(ev.key === 'ArrowUp'){
             boy.classList.add("jump");
 
         }
         else if(ev.key === 'ArrowRight'){
-            const distX = getDistance() + 30;
+            distX = getDistance() + 30;
+            if (distX > screenWidth) {
+                distX = screenWidth;
+            }
             boy.style.transform = `translateX(${distX}px) rotateY(0deg)` 
             boy.classList.add("jump");
         } 
         else if(ev.key === 'ArrowLeft'){
-            const distY = getDistance() + -30;
+            distY = getDistance() + -30;
+            if (distY < 0) {
+                distY = 0;
+            }
             boy.style.transform = `translateX(${distY}px) rotateY(180deg)` 
             boy.classList.add("jump");
         }
+        checkBorders();
     });
 
     boy.addEventListener("animationend", () => {
         boy.classList.remove("jump");
     });
 
-    console.log(getDistance());
+    const checkBorders = () => {
+        const playerX = boy.getBoundingClientRect().left;
+
+        if (playerX < 0) {
+            distY = 0;
+        } else if (playerX > screenWidth) {
+            distX = screenWidth;
+        } else {
+            distX = 0;
+        }
+    };
 }
-
-moveRight();
-
-
-
-const moving = () => { 
-    window.addEventListener("keydown", (ev) => {
-        if(ev.key === 'ArrowLeft'){
-            boy.classList.toggle("jump");
-        } 
-    });
-
-    boy.addEventListener("animationend", () => {
-        boy.classList.remove("jump");
-    });
-}
+moving();
