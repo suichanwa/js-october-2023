8a2000b44123fea55b19266f4d5de98910751c51
suichanwa <augustinnegura746@gmail.com>
2023-11-20 11:27:33 -0800

    adding convertor

---
+++ convertorapp/main.js
@@ -0,0 +1,16 @@
+import json from './current.json' assert { type: 'json' };
+
+window.convertCurrency = function() {
+    const amountInput = document.getElementById('amountInput').value;
+    const usdRate = json.current.USD;
+    const eurRate = json.current.EUR;
+    const jpyRate = json.current.JPY;
+
+    const convertedUsd = amountInput * usdRate;
+    const convertedEur = amountInput * eurRate;
+    const convertedJpy = amountInput * jpyRate;
+
+    document.getElementById('usdOutput').innerText = `Converted USD amount: ${convertedUsd.toFixed(2)}`;
+    document.getElementById('eurOutput').innerText = `Converted EUR amount: ${convertedEur.toFixed(2)}`;
+    document.getElementById('jpyOutput').innerText = `Converted JPY amount: ${convertedJpy.toFixed(2)}`;
+}
